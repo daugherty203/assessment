613c8d0011f73ee5fc7861990e537a450dcc8844
Darlene Daugherty <darlenedaugherty@MacBook-Pro-3.local>
2018-07-24 19:56:09 -0600

    assess

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,29 @@ import Flash from './Flash';
 import Home from './Home';
 import { Switch, Route } from 'react-router-dom';
 import { Segment } from 'semantic-ui-react';
+import {beers} from '/beers';
+import {breweries} from '/beers';
+import axios from 'axios'
 
 class App extends Component {
+
+componentDidMount () {
+  axios.get =('https://api/all_beers.'
+  )
+  .then((res) => {       
+    this.setState({data:res.all.beers});
+  })
+  axios.get =('https://api/all_breweries')
+  .then((res) => {       
+    this.setState({data:res.all.breweries});
+
+  })
+
+  }
+
+
+
+
   render() {
     return (
       <Segment style={styles.background}>
@@ -15,6 +36,8 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route component={NoMatch} />
+          <Route component={beers} />
+          <Route component={breweries} />
         </Switch>
       </Segment>
     );
